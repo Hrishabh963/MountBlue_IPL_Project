9b751fb0590b607cddcf1f7a267896b51e02436c
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-04 09:51:19 +0530

    Added README.md

--- _tests_/matchesPerYear.test.js
+++ _tests_/matchesPerYear.test.js
@@ -1,43 +1,44 @@
 const matchesPerYear = require('../src/server/matchesPerYear');
 
-const testDataSet = [{
-        "season": 2008,
-        "team1": "Sunrisers Hyderabad",
-        "team2": "Royal Challengers Bangalore",
-    },
-    {
-        "season": 2017,
-        "team1": "Mumbai Indians",
-        "team2": "Rising Pune Supergiant",
-    },
-    {
-        "season": 2017,
-        "team1": "Gujarat Lions",
-        "team2": "Kolkata Knight Riders",
-    },
-    {
-        "season": 2009,
-        "team1": "Rising Pune Supergiant",
-        "team2": "Kings XI Punjab",
-    }
-]
+const testDataSet = [
+  {
+    season: 2008,
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+  },
+  {
+    season: 2017,
+    team1: 'Mumbai Indians',
+    team2: 'Rising Pune Supergiant',
+  },
+  {
+    season: 2017,
+    team1: 'Gujarat Lions',
+    team2: 'Kolkata Knight Riders',
+  },
+  {
+    season: 2009,
+    team1: 'Rising Pune Supergiant',
+    team2: 'Kings XI Punjab',
+  },
+];
 
 const testDataResult = {
-    '2008': 1,
-    '2009': 1,
-    '2017': 2
+  2008: 1,
+  2009: 1,
+  2017: 2,
 };
 
 test('Result generated should match the testDataResult ', () => {
-    expect(matchesPerYear(testDataSet)).toStrictEqual(testDataResult);
-})
+  expect(matchesPerYear(testDataSet)).toStrictEqual(testDataResult);
+});
 
 test('Year 2017 should be 2 matches', () => {
-    const generatedResult = matchesPerYear(testDataSet);
-    expect(generatedResult[2017]).toBe(2)
-})
+  const generatedResult = matchesPerYear(testDataSet);
+  expect(generatedResult[2017]).toBe(2);
+});
 
 test('Year 2010 should be undefined', () => {
-    const generatedResult = matchesPerYear(testDataSet);
-    expect(generatedResult[2010]).toBe(undefined)
-})
+  const generatedResult = matchesPerYear(testDataSet);
+  expect(generatedResult[2010]).toBe(undefined);
+});
--- _tests_/matchesWonPerTeamPerYear.test.js
+++ _tests_/matchesWonPerTeamPerYear.test.js
@@ -1,49 +1,50 @@
 const countMatchesWonByTeamsPerYear = require('../src/server/matchesWonPerTeamPerYear');
 
-const testDataSet = [{
-        "season": 2008,
-        "winner": "Kings XI Punjab",
-    },
-    {
-        "season": 2008,
-        "winner": "Delhi Daredevils"
-    },
-    {
-        "season": 2017,
-        "winner": "Mumbai Indians",
-    },
-    {
-        "season": 2011,
-        "winner": "Kolkata Knight Riders",
-    },
-    {
-        "season": 2017,
-        "winner": "Mumbai Indians",
-    }
-]
+const testDataSet = [
+  {
+    season: 2008,
+    winner: 'Kings XI Punjab',
+  },
+  {
+    season: 2008,
+    winner: 'Delhi Daredevils',
+  },
+  {
+    season: 2017,
+    winner: 'Mumbai Indians',
+  },
+  {
+    season: 2011,
+    winner: 'Kolkata Knight Riders',
+  },
+  {
+    season: 2017,
+    winner: 'Mumbai Indians',
+  },
+];
 
 const testDataResult = {
-    '2008': { 'Delhi Daredevils': 1, 'Kings XI Punjab': 1 },
-    '2011': { 'Kolkata Knight Riders': 1 },
-    '2017': { 'Mumbai Indians': 2 }
-}
+  2008: { 'Delhi Daredevils': 1, 'Kings XI Punjab': 1 },
+  2011: { 'Kolkata Knight Riders': 1 },
+  2017: { 'Mumbai Indians': 2 },
+};
 
 test('For year 2008 Delhi Darevils should win 1 ', () => {
-    const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
-    expect(resultsGenerated[2008]['Delhi Daredevils']).toBe(1);
-})
+  const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
+  expect(resultsGenerated[2008]['Delhi Daredevils']).toBe(1);
+});
 
 test('For year 2017 Mumbai Indians should win 2 ', () => {
-    const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
-    expect(resultsGenerated[2017]['Mumbai Indians']).toBe(2);
-})
+  const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
+  expect(resultsGenerated[2017]['Mumbai Indians']).toBe(2);
+});
 
 test('For year 2011 Mumbai Indians should be undefined', () => {
-    const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
-    expect(resultsGenerated[2011]['Mumbai Indians']).toBe(undefined);
-})
+  const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
+  expect(resultsGenerated[2011]['Mumbai Indians']).toBe(undefined);
+});
 
 test('Result generated should match testDataResult', () => {
-    const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
-    expect(resultsGenerated).toStrictEqual(testDataResult);
-})
+  const resultsGenerated = countMatchesWonByTeamsPerYear(testDataSet);
+  expect(resultsGenerated).toStrictEqual(testDataResult);
+});
