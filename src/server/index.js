990244755f89ab9e7d08ce39f33f8e368c8dd2b6
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-04 18:18:23 +0530

    Added some comments

--- src/server/index.js
+++ src/server/index.js
@@ -13,15 +13,19 @@ function main() {
     if (error) {
       console.log(`Error reading the data file => ${error}`);
     } else {
+      //Function to count number of matches per season
       const matchesPerYear = countMatchesPerYear(matches);
       writeFileData(matchesPerYear, 'matchesPerYear');
 
+      //Function to count wins of each individual team sorted by year
       const matchesWonPerTeamPerYear = countMatchesWonByTeamsPerYear(matches);
       writeFileData(matchesWonPerTeamPerYear, 'matchesWonPerTeamPerYear');
 
+      //Function to count the number of times a team won the toss and the match
       const tossWinMatchWin = tossWinMatchWinCount(matches);
       writeFileData(tossWinMatchWin, 'tossWinMatchWin');
 
+      //Function to count the player of match each season
       const playerOfMatchPerSeason = findPlayerOfMatch(matches);
       writeFileData(playerOfMatchPerSeason, 'playerOfMatchPerSeason');
 
@@ -29,12 +33,15 @@ function main() {
         if (error) {
           console.log(`Error reading the data file => ${error}`);
         } else {
+          //Function to count extra runs secured by each team
           const extraRunsPerTeam = countExtraRunsPerTeam(matches, deliveries);
           writeFileData(extraRunsPerTeam, 'extraRunsPerTeamIn2016');
 
+          //Function to calculate economy rates and return top 10 best bowlers
           const topTenEconomyBowlers = topEconomyBowlers(matches, deliveries);
           writeFileData(topTenEconomyBowlers, 'topTenEconomyBowlers');
 
+          //Function to count strike rates of each batsman per season
           const strikeRates = findStrikeRate(matches, deliveries);
           writeFileData(strikeRates, 'strikeRatesForEachSeason');
         }
