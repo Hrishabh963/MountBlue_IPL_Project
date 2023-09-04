1832273150d3946308d9453d92a600696d4e7804
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-04 11:41:02 +0530

    Created test cases for extraRunsPerTeamIn2016

---
+++ _tests_/extraRunsPerTeamIn2016.test.js
@@ -0,0 +1,40 @@
+const countExtraRunsPerTeam = require('../src/server/extraRunsPerTeamIn2016');
+const testRunData = [
+    { "match_id": "1", "inning": "1", "batting_team": "Sunrisers Hyderabad", "extra_runs": "0" },
+    { "match_id": "1", "inning": "1", "batting_team": "Sunrisers Hyderabad", "extra_runs": "2" },
+    { "match_id": "1", "inning": "1", "batting_team": "Sunrisers Hyderabad", "extra_runs": "1" },
+    { "match_id": "1", "inning": "1", "batting_team": "Sunrisers Hyderabad", "extra_runs": "3" },
+    { "match_id": "1", "inning": "2", "batting_team": "Royal Challengers Bangalore", "extra_runs": "0" },
+    { "match_id": "1", "inning": "2", "batting_team": "Royal Challengers Bangalore", "extra_runs": "1" },
+    { "match_id": "1", "inning": "2", "batting_team": "Royal Challengers Bangalore", "extra_runs": "2" },
+]
+
+const testMatchData = [{
+    "id": 1,
+    "season": 2016,
+    "team1": "Sunrisers Hyderabad",
+    "team2": "Royal Challengers Bangalore",
+    "winner": "Sunrisers Hyderabad",
+    "win_by_runs": 35,
+    "win_by_wickets": 0,
+}]
+
+const testDataResult = {
+    'Sunrisers Hyderabad': 6,
+    'Royal Challengers Bangalore': 3
+}
+
+test('Sunrisers Hyderabad should have 6 extra runs ', () => {
+    const resultGenerated = countExtraRunsPerTeam(testMatchData, testRunData);
+    expect(resultGenerated['Sunrisers Hyderabad']).toBe(6);
+})
+
+test('Royal Challengers Bangalore should have 3 extra runs ', () => {
+    const resultGenerated = countExtraRunsPerTeam(testMatchData, testRunData);
+    expect(resultGenerated['Royal Challengers Bangalore']).toBe(3);
+})
+
+test('Testdata should match expected data ', () => {
+    const resultGenerated = countExtraRunsPerTeam(testMatchData, testRunData);
+    expect(resultGenerated).toStrictEqual(testDataResult);
+})
